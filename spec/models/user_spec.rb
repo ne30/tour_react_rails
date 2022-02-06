608c1c9e43447862e4ca83bc355bd92c23020a3f
ne30 <neeraj.ace.30@gmail.com>
2022-02-06 18:02:27 +0530

    before coverage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,8 +5,25 @@ RSpec.describe User, type: :model do
          gender: 0, password: "rspec",\
          password_confirmation: "rspec",\
          is_admin: false) }
+
     before { subject.save }
+    
     it 'checking user name' do
         expect(subject.user_name).to eq("rspec1")
     end
+
+    it 'valid with valid data' do
+        expect(subject).to be_valid
+    end
+
+    it "is not valid without a user_name" do
+        temp_user = User.new(user_name: nil)
+        expect(temp_user).to_not be_valid
+    end
+
+    it "is not valid without a gender" do
+        temp_user = User.new(gender: nil)
+        expect(temp_user).to_not be_valid
+    end
+
 end
